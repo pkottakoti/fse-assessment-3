a2a642bc9cf2fda16734622b7c4afaa21eae0897
deep6761 <deepu0424@gmail.com>
2019-05-10 14:48:34 -0400

    added all testcases

--- src/test/java/alisnobba/eventService/ServiceTests/EventServiceTest.java
+++ src/test/java/alisnobba/eventService/ServiceTests/EventServiceTest.java
@@ -1,19 +1,9 @@
 package alisnobba.eventService.ServiceTests;
 
-import alisnobba.eventService.model.Event;
-import alisnobba.eventService.repository.EventRepository;
-import alisnobba.eventService.service.EventService;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
+
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 @RunWith(MockitoJUnitRunner.class)
 public class EventServiceTest {
 }
